4589f3e262dd795ef72e6674b6aeaf2876e9340f
AhmedKhalil777 <progeng_Ahmed_Khalil@outlook.com>
2020-05-18 06:46:29 +0200

    Add Dotnetcore MVC APP

---
+++ ChatApp.MVC/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.MVC.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SentToAll(string message ,  string sender)=>
+            await Clients.All.SendAsync("Broadcast", message, sender);
+        
+    }
+}
